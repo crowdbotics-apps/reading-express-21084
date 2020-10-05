fcb3cc7e883120a23129354d70979a82e73dea7a
Crowdbotics <team@crowdbotics.com>
2020-10-05 01:35:21 +0000

    Update models

--- backend/reading_express_21084/wsgi.py
+++ backend/reading_express_21084/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'reading_express_21084.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "reading_express_21084.settings")
 
 application = get_wsgi_application()
